c5b1795e1d82822e149520aa9c7a46e6ea75903f
Hector Rivas Gandara <keymon@gmail.com>
2017-05-23 11:56:59 +0100

    Simplify a little bit more

--- some_module/template.tf
+++ some_module/template.tf
@@ -4,38 +4,16 @@ data "template_file" "example_template" {
 
   vars {
     container_env = "${
-      join (
-        format(",\n      "),
-        concat(
-          null_resource._jsonencode_container_env.*.triggers.entries
-        )
-      )
-    }${
-      (
-        length(null_resource._jsonencode_container_env.*.triggers.entries)
-      ) != 0 ? "," : ""
+      join (",",  null_resource._jsonencode_container_env.*.triggers.entries)
     }"
   }
 
   depends_on = [ "null_resource._jsonencode_container_env"]
 }
 
-# Create a json snippet with the list of variables
-#
-# It will use a null_resource to generate a list of json encoded
-# name-value maps like {"name": "...", "value": "..."}, and then
-# we join them in a data template file.
-#
 resource "null_resource" "_jsonencode_container_env" {
   triggers {
-    entries = "${
-      jsonencode(
-        map(
-          "name", element(keys(var.container_env), count.index),
-          "value", element(values(var.container_env), count.index),
-          )
-      )
-    }"
+    entries = "${element(values(var.container_env), count.index)}"
   }
   count = "${length(var.container_env)}"
 }
